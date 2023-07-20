73fae8342405d1f799408977923b0e5ac072c437
Milko Škofič <milko@skofic.io>
2023-07-20 16:23:31 +0200

    Fixed file names and other details. Running PREPARE.sh.

--- Chelsa/1981-2010/workflow/PREPARE.sh
+++ Chelsa/1981-2010/workflow/PREPARE.sh
@@ -5,11 +5,34 @@
 ###
 start=$(date +%s)
 
-./01.download.sh
-./02.clip.sh
-./03.convert.sh
+###
+# Find default parameters.
+###
+conf=$(dirname "$(realpath  "$BASH_SOURCE")")
+conf="$(dirname "$conf")"
+conf="$(dirname "$conf")"
+conf="$(dirname "$conf")"
+conf="${conf}/config.txt"
+
+###
+# Load default parameters.
+###
+source "$conf"
+
+###
+# Run workflow scripts.
+###
+cmd="${path}/Chelsa/1981-2010/workflow/01.download.sh"
+$cmd
+
+cmd="${path}/Chelsa/1981-2010/workflow/02.clip.sh"
+$cmd
+
+cmd="${path}/Chelsa/1981-2010/workflow/03.convert.sh"
+$cmd
 
 end=$(date +%s)
 elapsed=$((end-start))
-echo "==>: $elapsed seconds"
+echo ""
+echo "TOTAL TIME: $elapsed seconds"
 echo ""
