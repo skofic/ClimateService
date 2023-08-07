08ff3591e5e54114484b0d9985d567838e58ab72
Milko Škofič <milko@skofic.io>
2023-08-07 17:07:42 +0200

    Testing concurrent convert.

--- WorldClim/config/test.sh
+++ WorldClim/config/test.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 ###
-# Prepare data.
-# Concurrent version.
-#
-# Download GeoIFF maps,
-# clip maps to useful region,
-# convert maps to CSV files
+# Prepare data: download, clip to region and convert to GeoJSON.
 ###
 
 ###
@@ -18,12 +13,43 @@ source "${HOME}/.ClimateService"
 # Globals.
 ###
 epoc="${path}/WorldClim/1970-2000"
-cmd="${epoc}/workflow/PREPARE_concurrent.sh"
+
+echo "**************************************************"
+echo "*** PREPARE.sh"
+echo "**************************************************"
+PREPARE_START=$(date +%s)
+	
+# ###
+# # Download full maps.
+# ###
+# cmd="${epoc}/workflow/download_concurrent.sh"
+# $cmd
+# if [ $? -ne 0 ]
+# then
+# 	echo "*************"
+# 	echo "*** ERROR ***"
+# 	echo "*************"
+# 	exit 1
+# fi
+
+# ###
+# # Clip full maps to EUFGIS region.
+# ###
+# cmd="${epoc}/workflow/clip_concurrent.sh"
+# $cmd
+# if [ $? -ne 0 ]
+# then
+# 	echo "*************"
+# 	echo "*** ERROR ***"
+# 	echo "*************"
+# 	exit 1
+# fi
 
 ###
-# Execute script.
+# Convert clipped maps to CSV (longitude, latitude and value).
 ###
-$cmd | tee "${epoc}/log/1_PREPARE_concurrent.log"
+cmd="${epoc}/workflow/convert_concurrent.sh"
+$cmd
 if [ $? -ne 0 ]
 then
 	echo "*************"
@@ -31,3 +57,11 @@ then
 	echo "*************"
 	exit 1
 fi
+
+PREPARE_END=$(date +%s)
+elapsed=$((PREPARE_END-PREPARE_START))
+echo ""
+echo "**************************************************"
+echo "*** PREPARE.sh - TOTAL TIME: $elapsed seconds"
+echo "**************************************************"
+echo ""
