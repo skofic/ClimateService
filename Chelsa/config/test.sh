9b3aa4a9049f62d6aa5c13006ce2ceccf441747f
Milko Škofič <milko@skofic.io>
2023-08-20 13:09:53 +0200

    Forgot to create the 1_PREPARE_concurrent.sh scripts.

--- Chelsa/config/test.sh
+++ Chelsa/config/test.sh
@@ -1,110 +1,142 @@
 #!/bin/sh
 
 ###
-# Create Chelsa period data.
-#
-# This script will create everything, it is supposed to be run
-# just after cloning the git repository.
-#
-# Be aware that this involves downloading more than 200GB. of data
-# and processing over 8 billion records: this means that the whole
-# process will probably take more than a week, or even much more,
-# depending on the computer that runs it.
+# Create Chelsa database.
+# Concurrent version.
 ###
 
 ###
 # Load default parameters.
 ###
 source "${HOME}/.ClimateService"
+CHELSA=$(date +%s)
 
 ###
-# Globals.
-###
-epoc="${path}/Chelsa/1981-2010"
-
-echo "**************************************************"
-echo "**************************************************"
-echo "*** create_1981_2010.sh"
-echo "**************************************************"
-echo "**************************************************"
-CREATE_1981_2010_START=$(date +%s)
-
-###
-# Create logs directory and remove eventual existing logs.
-###
-mkdir "${epoc}/log/"
-
-# ###
-# # Prepare data.
-# ###
-# cmd="${epoc}/workflow/1_PREPARE.sh"
-# $cmd
-# if [ $? -ne 0 ]
-# then
-# 	echo "*************"
-# 	echo "*** ERROR ***"
-# 	echo "*************"
-# 	exit 1
-# fi
-
-# ###
-# # Process data.
-# ###
-# cmd="${epoc}/workflow/2_PROCESS.sh"
-# $cmd
-# if [ $? -ne 0 ]
-# then
-# 	echo "*************"
-# 	echo "*** ERROR ***"
-# 	echo "*************"
-# 	exit 1
-# fi
-
-# ###
-# # Combine bioclimatic data.
-# ###
-# cmd="${epoc}/workflow/3_COMBINE_ANNUAL.sh"
-# $cmd
-# if [ $? -ne 0 ]
-# then
-# 	echo "*************"
-# 	echo "*** ERROR ***"
-# 	echo "*************"
-# 	exit 1
-# fi
-
-###
-# Combine monthly data.
-###
-cmd="${epoc}/workflow/4_COMBINE_MONTHLY.sh"
-$cmd
-if [ $? -ne 0 ]
-then
-	echo "*************"
-	echo "*** ERROR ***"
-	echo "*************"
-	exit 1
-fi
-
-###
-# Merge data.
-###
-cmd="${epoc}/workflow/5_MERGE.sh"
-$cmd
-if [ $? -ne 0 ]
-then
-	echo "*************"
-	echo "*** ERROR ***"
-	echo "*************"
-	exit 1
-fi
-
-CREATE_1981_2010_END=$(date +%s)
-elapsed=$((CREATE_1981_2010_END-CREATE_1981_2010_START))
+# Create directories.
+##
 echo ""
-echo "**************************************************"
-echo "**************************************************"
-echo "*** create_1981_2010.sh - TOTAL TIME: $elapsed seconds"
-echo "**************************************************"
-echo "**************************************************"
+echo "<<< CREATE DIRECTORIES >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/create_directories.sh"
+$cmd
+
+# ###
+# # 1981-2010.
+# ###
+# echo ""
+# echo "<<< PERIOD 1981-2010 >>>"
+# echo ""
+# cmd="${path}/Chelsa/workflow/create_1981_2010_concurrent.sh"
+# $cmd | tee "${path}/Chelsa/log/1981_2010.log"
+# if [ $? -ne 0 ]
+# then
+# 	echo "*************"
+# 	echo "*** ERROR ***"
+# 	echo "*************"
+# 	exit 1
+# fi
+
+###
+# 2011-2040.
+###
+echo ""
+echo "<<< PERIOD 2011-2040 >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/create_2011_2040_concurrent.sh"
+$cmd | tee "${path}/Chelsa/log/2011_2040.log"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+###
+# 2041-2070.
+###
+echo ""
+echo "<<< PERIOD 2041-2070 >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/create_2041_2070_concurrent.sh"
+$cmd | tee "${path}/Chelsa/log/2041_2070.log"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+###
+# 2071-2100.
+###
+echo ""
+echo "<<< PERIOD 2071-2100 >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/create_2071_2100_concurrent.sh"
+$cmd | tee "${path}/Chelsa/log/2071_2100.log"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+###
+# Load dumps into temporary period collections
+# and dump periods coordinates.
+###
+echo ""
+echo "<<< LOAD PERIOD DUMPS INTO DATABASE >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/load_periods.sh"
+$cmd | tee "${path}/Chelsa/log/load_periods.log"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+###
+# Load coordinate dumps into Chelsa map collection.
+###
+echo ""
+echo "<<< LOAD COORDINATES INTO DATABASE >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/load_map.sh"
+$cmd | tee "${path}/Chelsa/log/load_map.log"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+###
+# Dump Chelsa and dump Chelsa map.
+###
+echo ""
+echo "<<< DUMP AND LOAD IN DATABASE >>>"
+echo ""
+cmd="${path}/Chelsa/workflow/dump_chelsa.sh"
+$cmd | tee "${path}/Chelsa/log/dump_chelsa.log"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+CHELSA_END=$(date +%s)
+elapsed=$((CHELSA_END-CHELSA_START))
+echo ""
+echo "==>==>==>==>==>==>==>==>==>==>==>==>==>==>==>==>==>"
+echo "==> CHELSA.sh - TOTAL TIME: $elapsed seconds"
+echo "==>==>==>==>==>==>==>==>==>==>==>==>==>==>==>==>==>"
 echo ""
