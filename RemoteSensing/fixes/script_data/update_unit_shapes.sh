8a8777d5a24eb37c33cd244baa637b82701794eb
Milko Škofič <milko@skofic.io>
2023-08-22 22:05:18 +0200

    Finalised the UnitShapes collection fix.

---
+++ RemoteSensing/fixes/script_data/update_unit_shapes.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+###
+# Update unit shapes (UnitShapes) variable names.
+#
+# The script wil:
+# - Update the variable names.
+# - Convert the shape reference from an _id to a _key.
+#
+# The script expects the following parameters:
+# - $1: Collection name.
+#
+# You should delete the old indexes, before running the script,
+# and re-create the indexes, with the new variable names,
+# after running the script.
+###
+
+###
+# Load default parameters.
+###
+source "${HOME}/.ClimateService"
+
+###
+# Set path parameters.
+###
+epoc="$path/RemoteSensing/fixes"
+
+###
+# Dump updated GeometryID name and create properties struct.
+###
+arangoexport \
+	--server.endpoint "$host" \
+	--server.database "$base" \
+	--server.username "$user" \
+	--server.password "$pass" \
+	--output-directory "$expo" \
+	--custom-query-file "${epoc}/script_query/update_unit_shapes.aql" \
+	--custom-query-bindvars "{\"@@collection\": \"$1\"}" \
+	--compress-output true \
+	--overwrite true \
+	--type "jsonl"
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+###
+# Import data from JSONL file.
+###
+arangoimport \
+	--server.endpoint "$host" \
+	--server.database "$base" \
+	--server.username "$user" \
+	--server.password "$pass" \
+	--file "${expo}/query.jsonl.gz" \
+	--type "jsonl" \
+	--collection "$1" \
+	--create-collection true \
+	--create-collection-type "document" \
+	--auto-rate-limit true \
+	--overwrite true
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
