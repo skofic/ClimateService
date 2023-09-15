c6cb827abab2ac60f16d8fb14e35679f7807621c
Milko Škofič <milko@skofic.io>
2023-09-15 09:39:43 +0200

    Created FAPAN download list.

---
+++ DroughtObservatory/FAPAN/workflow/download_list.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+###
+# Print list of files to download.
+###
+
+###
+# Load default parameters.
+###
+source "${HOME}/.ClimateService"
+
+###
+# Globals.
+###
+fol="FAPAN"
+ini=2012
+end=2022
+epoc="${path}/DroughtObservatory/${fol}"
+
+echo "--------------------------------------------------"
+echo "- DOWNLOAD ${fol} FILES"
+echo "--------------------------------------------------"
+
+###
+# Iterate years.
+###
+for year in $(seq ${ini} 1 ${end})
+do
+
+	###
+	# Set parameters.
+	###
+	url="https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/DROUGHTOBS/Drought_Observatories_datasets/EDO_Fraction_of_Absorbed_Photosynthetically_Active_Radiation_Anomalies_fAPAR/ver1-3-2/fapan_m_euu_${year}0101_${year}1221_t.zip"
+	command="wget --continue $url --output-document=\"${epoc}/download/${year}.zip\""
+	echo "$command"
+
+done
