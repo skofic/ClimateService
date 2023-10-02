daaab61bfd2f1c77741629648efa31f1b020326e
Milko Škofič <milko@skofic.io>
2023-10-02 09:37:18 +0200

    Trying to merge files, merging map files only.

--- DroughtObservatory/config/test.sh
+++ DroughtObservatory/config/test.sh
@@ -83,30 +83,14 @@ fi
 # 	exit 1
 # fi
 # 
-# ###
-# # Merge map files.
-# ###
-# echo ""
-# echo "<<< MERGE MAP FILES >>>"
-# echo ""
-# cmd="${epoc}/workflow/merge_map.sh"
-# $cmd | tee "${epoc}/log/4_merge_map.log"
-# if [ $? -ne 0 ]
-# then
-# 	echo "*************"
-# 	echo "*** ERROR ***"
-# 	echo "*************"
-# 	exit 1
-# fi
-
 ###
 # Merge map files.
 ###
 echo ""
-echo "<<< MERGE DATA FILES >>>"
+echo "<<< MERGE MAP FILES >>>"
 echo ""
-cmd="${epoc}/workflow/merge_data.sh"
-$cmd | tee "${epoc}/log/5_merge_data.log"
+cmd="${epoc}/workflow/merge_map.sh"
+$cmd | tee "${epoc}/log/4_merge_map.log"
 if [ $? -ne 0 ]
 then
 	echo "*************"
@@ -114,6 +98,22 @@ then
 	echo "*************"
 	exit 1
 fi
+
+# ###
+# # Merge map files.
+# ###
+# echo ""
+# echo "<<< MERGE DATA FILES >>>"
+# echo ""
+# cmd="${epoc}/workflow/merge_data.sh"
+# $cmd | tee "${epoc}/log/5_merge_data.log"
+# if [ $? -ne 0 ]
+# then
+# 	echo "*************"
+# 	echo "*** ERROR ***"
+# 	echo "*************"
+# 	exit 1
+# fi
 
 CREATE_concurrent_END=$(date +%s)
 elapsed=$((CREATE_concurrent_END-CREATE_concurrent_TART))
