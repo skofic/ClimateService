f36375aa51424e8e1b614ab4bf9c3c763d948fd9
Milko Škofič <milko@skofic.io>
2023-11-25 22:28:16 +0100

    Fixed Chelsa monthly precipitation: need to harmonise with env_climate_pr unit.

--- Chelsa/2071-2100/MPI-ESM1-2-HR/ssp370/script_data/process_monthly.sh
+++ Chelsa/2071-2100/MPI-ESM1-2-HR/ssp370/script_data/process_monthly.sh
@@ -27,55 +27,55 @@ coll="temp_ping"
 epoc="${path}/Chelsa/2071-2100/MPI-ESM1-2-HR/ssp370"
 cmd="${path}/Chelsa/script_data/process_monthly.sh"
 
-###
-# Iterate monthly precipitation,
-# divide value by 100.
-###
-for name in "pr"
-do
-
-	###
-	# Iterate months.
-	###
-	echo "====================================================================="
-	echo "= $name"
-	echo "====================================================================="
- 	for month in "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12"
-	do
-		echo "==> $name_$month"
-		start=$(date +%s)
-	
-		###
-		# Process variable name and scale to 0.01.
-		###
-		$cmd "${epoc}/CSV/${name}/${name}_${month}.csv.gz" \
-			 "${epoc}/data/${name}/${name}_${month}.csv.gz" \
-			 "${path}/Chelsa/script_query/process_scale001_month.aql" \
-			 "{\"@@collection\": \"$coll\", \"variable\": \"${pref}_${name}\", \"month\": \"$month\"}" \
-			 "$coll" \
-			 "${pref}_${name}"
-		if [ $? -ne 0 ]
-		then
-			echo "*************"
-			echo "*** ERROR ***"
-			echo "*************"
-			exit 1
-		fi
-		
-		end=$(date +%s)
-		elapsed=$((end-start))
-		echo "--------------------------------------------------"
-		echo "2071-2100 process_monthly.sh (${name}_${month} scale 0.01)"
-		echo "Elapsed time: $elapsed seconds"
-		echo "----------------------------------------"
-	done
-	
-done
+# ###
+# # Iterate monthly precipitation,
+# # divide value by 100.
+# ###
+# for name in "pr"
+# do
+# 
+# 	###
+# 	# Iterate months.
+# 	###
+# 	echo "====================================================================="
+# 	echo "= $name"
+# 	echo "====================================================================="
+#  	for month in "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12"
+# 	do
+# 		echo "==> $name_$month"
+# 		start=$(date +%s)
+# 	
+# 		###
+# 		# Process variable name and scale to 0.01.
+# 		###
+# 		$cmd "${epoc}/CSV/${name}/${name}_${month}.csv.gz" \
+# 			 "${epoc}/data/${name}/${name}_${month}.csv.gz" \
+# 			 "${path}/Chelsa/script_query/process_scale001_month.aql" \
+# 			 "{\"@@collection\": \"$coll\", \"variable\": \"${pref}_${name}\", \"month\": \"$month\"}" \
+# 			 "$coll" \
+# 			 "${pref}_${name}"
+# 		if [ $? -ne 0 ]
+# 		then
+# 			echo "*************"
+# 			echo "*** ERROR ***"
+# 			echo "*************"
+# 			exit 1
+# 		fi
+# 		
+# 		end=$(date +%s)
+# 		elapsed=$((end-start))
+# 		echo "--------------------------------------------------"
+# 		echo "2071-2100 process_monthly.sh (${name}_${month} scale 0.01)"
+# 		echo "Elapsed time: $elapsed seconds"
+# 		echo "----------------------------------------"
+# 	done
+# 	
+# done
 
 ###
 # Iterate monthly temperature.
 ###
-for name in "tas" "tasmax" "tasmin"
+for name in "pr" "tas" "tasmax" "tasmin"
 do
 
 	###
