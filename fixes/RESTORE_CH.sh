694c4ae3bf07041abe39a45f172351b03282a42d
Milko Škofič <milko@skofic.io>
2023-12-27 16:43:07 +0100

    Finalised the import script for ShapeData and DroughtObservatory.

--- fixes/RESTORE_CH.sh
+++ fixes/RESTORE_CH.sh
@@ -14,8 +14,8 @@ source "${HOME}/.GeoService"
 ###
 collection_rs="ShapeData"
 collection_do="DroughtObservatory"
-bdir_rs="home/geouser/Backup/RemoteSensing"
-bdir_do="home/geouser/Backup/DroughtObservatory"
+bdir_rs="/home/geouser/Backup/RemoteSensing"
+bdir_do="/home/geouser/Backup/DroughtObservatory"
 
 ###
 # Parameters.
@@ -23,80 +23,77 @@ bdir_do="home/geouser/Backup/DroughtObservatory"
 dump_rs="${bdir_rs}/${collection_rs}.jsonl.gz"
 dump_do="${bdir_do}/${collection_do}.jsonl.gz"
 
-echo "dump_rs"
-echo "dump_do"
+echo "**************************************************"
+echo "**************************************************"
+echo "*** RESTORE_CH.sh"
+echo "**************************************************"
+echo "**************************************************"
+RESTORE_DATA_START=$(date +%s)
 
-# echo "**************************************************"
-# echo "**************************************************"
-# echo "*** 0_restore_data.sh"
-# echo "**************************************************"
-# echo "**************************************************"
-# RESTORE_DATA_START=$(date +%s)
-# 
-# echo ""
-# echo "**************************************************"
-# echo "*** Restore Chelsa geometries"
-# echo "*** from ${path}/${dump_map_name}.jsonl.gz"
-# echo "**************************************************"
-# 
-# ###
-# # Import data from JSONL file.
-# ###
-# arangoimport \
-# 	--server.endpoint "$host" \
-# 	--server.database "GeoService" \
-# 	--server.username "$user" \
-# 	--server.password "$pass" \
-# 	--file "$mdump" \
-# 	--type "jsonl" \
-# 	--collection "$collection_map" \
-# 	--create-collection true \
-# 	--create-collection-type "document" \
-# 	--auto-rate-limit true \
-# 	--overwrite true
-# if [ $? -ne 0 ]
-# then
-# 	echo "*************"
-# 	echo "*** ERROR ***"
-# 	echo "*************"
-# 	exit 1
-# fi
-# 
-# echo ""
-# echo "**************************************************"
-# echo "*** Restore Chelsa properties"
-# echo "*** from ${path}/${dump_chelsa_name}.jsonl.gz"
-# echo "**************************************************"
-# 
-# ###
-# # Import data from JSONL file.
-# ###
-# arangoimport \
-# 	--server.endpoint "$host" \
-# 	--server.database "GeoService" \
-# 	--server.username "$user" \
-# 	--server.password "$pass" \
-# 	--file "$ddump" \
-# 	--type "jsonl" \
-# 	--collection "$collection_data" \
-# 	--create-collection true \
-# 	--create-collection-type "document" \
-# 	--auto-rate-limit true \
-# 	--overwrite true
-# if [ $? -ne 0 ]
-# then
-# 	echo "*************"
-# 	echo "*** ERROR ***"
-# 	echo "*************"
-# 	exit 1
-# fi
-# 
-# RESTORE_DATA_END=$(date +%s)
-# elapsed=$((RESTORE_DATA_END-RESTORE_DATA_START))
-# echo ""
-# echo "**************************************************"
-# echo "**************************************************"
-# echo "*** 0_restore_data.sh - TOTAL TIME: $elapsed seconds"
-# echo "**************************************************"
-# echo "**************************************************"
-# echo ""
+echo ""
+echo "**************************************************"
+echo "*** Restore ShapeData"
+echo "*** from $dump_rs"
+echo "**************************************************"
+
+###
+# Import data from JSONL file.
+###
+arangoimport \
+	--server.endpoint "$host" \
+	--server.database "$base" \
+	--server.username "$user" \
+	--server.password "$pass" \
+	--file "$dump_rs" \
+	--type "jsonl" \
+	--collection "$collection_rs" \
+	--create-collection true \
+	--create-collection-type "document" \
+	--auto-rate-limit true \
+	--overwrite true
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+echo ""
+echo "**************************************************"
+echo "*** Restore DroughtObservatory"
+echo "*** from $dump_do"
+echo "**************************************************"
+
+###
+# Import data from JSONL file.
+###
+arangoimport \
+	--server.endpoint "$host" \
+	--server.database "$base" \
+	--server.username "$user" \
+	--server.password "$pass" \
+	--file "$dump_do" \
+	--type "jsonl" \
+	--collection "$collection_do" \
+	--create-collection true \
+	--create-collection-type "document" \
+	--auto-rate-limit true \
+	--overwrite true
+if [ $? -ne 0 ]
+then
+	echo "*************"
+	echo "*** ERROR ***"
+	echo "*************"
+	exit 1
+fi
+
+RESTORE_DATA_END=$(date +%s)
+elapsed=$((RESTORE_DATA_END-RESTORE_DATA_START))
+echo ""
+echo "**************************************************"
+echo "**************************************************"
+echo "*** RESTORE_CH.sh - TOTAL TIME: $elapsed seconds"
+echo "**************************************************"
+echo "**************************************************"
+echo ""
